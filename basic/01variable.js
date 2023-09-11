c7fb11e0ea6c5dda4e4f0e09c6608370a80822fc
Rahul2544 <72144416+Rahul2544@users.noreply.github.com>
2023-09-12 01:25:19 +0530

    variable declare

---
+++ basic/01variable.js
@@ -0,0 +1,6 @@
+const accountId = 2544;
+let accountEmail = "rahulnmishra@gmail.com";
+var accountPassword = "2354";
+accountCity="mumbai";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
